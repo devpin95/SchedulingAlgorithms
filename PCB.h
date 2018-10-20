8c41f735e59ded09fd9e792bc608db3d812a44ee
Unknown <devpindroid@gmail.com>
2018-10-19 22:25:41 -0600

    Added value to keep track of context switching times

--- PCB.h
+++ PCB.h
@@ -5,10 +5,11 @@ struct PCB {
     int pid = -1;
     int arrival_time = -1;
     int finish_time = -1;
-    int response_time = -1;
+    double response_time = -1;
     int burst_time = -1;
     int remaining_burst_time = -1;
-    int waiting_time = 0;
+    double waiting_time = 0;
+    int context_switches = 0;
 };
 
 #endif //SCHEDULINGALGORITHMS_PCB_H
