5f2ea15d854f19447d14854131a5282336c61bc0
Unknown <devpindroid@gmail.com>
2018-10-26 21:17:11 -0600

    Added variable to count the number of times a process was switched in

--- PCB.h
+++ PCB.h
@@ -4,12 +4,14 @@
 struct PCB {
     int pid = -1;
     int arrival_time = -1;
-    int finish_time = -1;
+    double finish_time = -1;
     double response_time = -1;
     int burst_time = -1;
+    double turnaround_time = -1;
     int remaining_burst_time = -1;
     double waiting_time = 0;
     int context_switches = 0;
+    int real_context_switches = 0;
 };
 
 #endif //SCHEDULINGALGORITHMS_PCB_H
