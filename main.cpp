128867a056ba752454c54cc234c412fe2020e4c9
Unknown <devpindroid@gmail.com>
2018-10-28 20:22:54 -0600

    Accepts program args and uses pointer for scheduling classes

--- main.cpp
+++ main.cpp
@@ -9,15 +9,24 @@
 #include "SRJF.h"
 #include "RR.h"
 
-int main() {
+int main( int argc, char * argv[] ) {
     LinkedList ll;
-    int num_processes = 0;
-    std::string filename = "/home/devpin/CLionProjects/SchedulingAlgorithms/input100";
+    long algorithm = std::stol( argv[2] );
+
+    if ( argc <= 1 ) {
+        std::cout << "Error. Expecting a string and integer";
+        return 1;
+    }
+
+    std::cout << argv[1] << " using ";
+
+    std::string filename = argv[1];
     std::ifstream infile;
     infile.open(filename);
 
     if ( !infile ) {
-        std::cout << "Could not open file " << filename << std::endl;
+        std::cout << "Error. Could not open file " << filename << std::endl;
+        return 1;
     }
     else {
         std::string temp;
@@ -40,23 +49,48 @@ int main() {
                 pcb.remaining_burst_time = pcb.burst_time;
 
                 ll.insertAtBack(pcb);
-
-                ++num_processes;
             }
         }
+
         infile.close();
 
-        RR scheduler(ll);
-        scheduler.setQuantum( 20 );
-        scheduler.run();
+        ISchedule * scheduler;
 
-        //ll.printVals();
-        std::cout << std::endl << "Stats" << std::endl;
-        std::cout << "Avg Wait Time: " << scheduler.avgWaitTime() << std::endl;
-        std::cout << "Avg CPU Burst Time: " << scheduler.avgCPUBurstTime() << std::endl;
-        std::cout << "Avg Turnaround Time: " << scheduler.avgTurnaroundTime() << std::endl;
-        std::cout << "Avg Response Time: " << scheduler.avgResponseTime() << std::endl;
-        std::cout << "Context Switches: " << scheduler.numContextSwitches() << std::endl;
+        switch ( algorithm ) {
+            case 1:
+                std::cout << "First Come First Serve" << std::endl;
+                scheduler = new FCFS(ll);
+                scheduler->run();
+                //FCFS FCFSscheduler(ll);
+                break;
+            case 2:
+                std::cout << "Shortest Job First" << std::endl;
+                scheduler = new SJF(ll);
+                scheduler->run();
+                //SJF SJFscheduler(ll);
+                break;
+            case 3:
+                std::cout << "Shortest Remaining Job First" << std::endl;
+                scheduler = new SRJF(ll);
+                scheduler->run();
+                //SRJF SRJFscheduler(ll);
+                break;
+            case 4:
+                std::cout << "Round Robin Q=" << argv[3] << std::endl;
+                scheduler = new RR(ll);
+                scheduler->setQuantum( atoi( argv[3] ) );
+                scheduler->run();
+                //RR RRscheduler(ll);
+                //RRscheduler.setQuantum( std::stoi( argv[3] ) );
+                break;
+            default: break;
+        }
+
+//        std::cout << "Avg Wait Time: " << scheduler.avgWaitTime() << std::endl;
+//        std::cout << "Avg CPU Burst Time: " << scheduler.avgCPUBurstTime() << std::endl;
+//        std::cout << "Avg Turnaround Time: " << scheduler.avgTurnaroundTime() << std::endl;
+//        std::cout << "Avg Response Time: " << scheduler.avgResponseTime() << std::endl;
+//        std::cout << "Context Switches: " << scheduler.numContextSwitches() << std::endl;
     }
 
     return 0;
